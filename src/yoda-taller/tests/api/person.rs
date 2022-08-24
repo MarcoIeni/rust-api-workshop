4901886c7d6911e5f981370f4fe5d7933ef7c16c
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2022-08-24 14:40:35 +0200

    rename vars

--- src/yoda-taller/tests/api/person.rs
+++ src/yoda-taller/tests/api/person.rs
@@ -3,26 +3,32 @@ use yoda_taller::swapi::Person;
 use crate::helpers::swapi_mock::{empty_query_result, person_query_result};
 use crate::helpers::TestApp;
 
+// Call swapi to assert how tall darth vader is.
 #[tokio::test]
 async fn darth_vader_is_tall() {
     let app = TestApp::spawn().await;
     let name = "Darth Vader";
-    let expected_person = Person {
+    let darth_vader = Person {
         name: name.to_string(),
         height: "202".to_string(),
     };
-    let body = person_query_result(&expected_person);
-    app.swapi_server.mock_people_query(name, body).await;
-    let darth_vader = app.swapi_client.people_by_name(name).await.unwrap();
-    assert_eq!(darth_vader, vec![expected_person]);
+    let response_body = person_query_result(&darth_vader);
+    app.swapi_server
+        .mock_people_query(name, response_body)
+        .await;
+    let people = app.swapi_client.people_by_name(name).await.unwrap();
+    assert_eq!(people, vec![darth_vader]);
 }
 
+// Spock isn't a Star Wars character.
 #[tokio::test]
 async fn spock_is_not_found() {
     let app = TestApp::spawn().await;
-    let body = empty_query_result();
+    let response_body = empty_query_result();
     let name = "Spock";
-    app.swapi_server.mock_people_query(name, body).await;
-    let spock = app.swapi_client.people_by_name(name).await.unwrap();
-    assert!(spock.is_empty());
+    app.swapi_server
+        .mock_people_query(name, response_body)
+        .await;
+    let people = app.swapi_client.people_by_name(name).await.unwrap();
+    assert!(people.is_empty());
 }
