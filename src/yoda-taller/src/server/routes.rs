82085bb18781aa46f30c16c2936f5aef97f95d2b
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2022-08-24 18:10:08 +0200

    add timeout test

--- src/yoda-taller/src/server/routes.rs
+++ src/yoda-taller/src/server/routes.rs
@@ -1,17 +1,18 @@
 use std::sync::Arc;
 
-use axum::{extract::Path, Extension, Json};
+use axum::{extract::Path, http::StatusCode, Extension, Json};
 
-use crate::YodaTaller;
+use crate::{YodaTaller, YodaTallerError};
 
 pub async fn health_check() {}
 
 pub async fn taller_than(
     Path(person_name): Path<String>,
     Extension(yoda_taller): Extension<Arc<YodaTaller>>,
-) -> Json<YodaTallerResponse> {
-    let taller = yoda_taller.is_taller_than(&person_name).await.unwrap();
-    YodaTallerResponse { taller }.into()
+) -> Result<Json<YodaTallerResponse>, StatusCode> {
+    let taller = yoda_taller.is_taller_than(&person_name).await?;
+    let response = YodaTallerResponse { taller }.into();
+    Ok(response)
 }
 
 #[derive(Debug, serde::Serialize)]
@@ -19,4 +20,13 @@ pub async fn taller_than(
 #[cfg_attr(feature = "test_fixture", derive(serde::Deserialize))]
 pub struct YodaTallerResponse {
     pub taller: bool,
+}
+
+impl From<YodaTallerError> for StatusCode {
+    fn from(e: YodaTallerError) -> Self {
+        match e {
+            YodaTallerError::HeightNotFound(_) => StatusCode::NOT_FOUND,
+            YodaTallerError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
 }
--- src/yoda-taller/tests/api/taller_server.rs
+++ src/yoda-taller/tests/api/taller_server.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use crate::helpers::swapi_mock::person_query_result;
 use yoda_taller::{server::routes::YodaTallerResponse, swapi::Person};
 
@@ -19,4 +21,24 @@ async fn yoda_is_not_taller_than_himself() {
 
     let body: YodaTallerResponse = response.json().await.unwrap();
     assert!(!body.taller);
+}
+
+#[ignore = "timeout takes too much"]
+#[tokio::test]
+async fn return_500_if_timeout() {
+    let app = TestApp::spawn().await;
+    let name = "Luke Skywalker";
+
+    let luke_mock = Person {
+        name: name.to_string(),
+        height: "172".to_string(),
+    };
+    let query_body = person_query_result(&luke_mock);
+
+    let delay = app.settings.swapi.timeout() + Duration::from_secs(1);
+    app.swapi_server
+        .mock_people_query_with_delay(name, query_body, delay)
+        .await;
+    let response = app.send_taller_req(name).await;
+    assert_eq!(500, response.status().as_u16());
 }
