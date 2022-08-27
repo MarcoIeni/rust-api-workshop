0219eeae6eb22250c11fd1c57051b2e9b0397a18
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2022-08-27 16:13:34 +0200

    add opentelemetry

--- src/traces/src/lib.rs
+++ src/traces/src/lib.rs
@@ -13,25 +13,38 @@ pub fn init_test_traces() {
 static TRACING: Lazy<()> = Lazy::new(|| {
     if std::env::var("ENABLE_LOGS").is_ok() {
         let default_filter_level = "info";
-        let subscriber = get_subscriber(default_filter_level);
+        let subscriber = get_subscriber(default_filter_level).unwrap();
         init_subscriber(subscriber);
     }
 });
 
-fn get_subscriber(env_filter: &str) -> impl Subscriber + Sync + Send {
+fn get_subscriber(env_filter: &str) -> anyhow::Result<impl Subscriber + Sync + Send> {
+    let tracer =
+        opentelemetry_jaeger::new_pipeline().install_batch(opentelemetry::runtime::Tokio)?;
+
+    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
     let env_filter =
         EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
     let formatting_layer = BunyanFormattingLayer::new("yoda-taller".to_string(), std::io::stdout);
-    Registry::default()
+    let registry = Registry::default()
         .with(env_filter)
         .with(JsonStorageLayer)
-        .with(formatting_layer)
+        .with(telemetry)
+        .with(formatting_layer);
+    Ok(registry)
 }
 
 /// Register a subscriber as global default to process span data.
 ///
 /// It should only be called once!
-pub fn init_subscriber(subscriber: impl Subscriber + Sync + Send) {
+fn init_subscriber(subscriber: impl Subscriber + Sync + Send) {
     LogTracer::init().expect("Failed to set logger");
     set_global_default(subscriber).expect("Failed to set subscriber");
+}
+
+pub fn init_prod_traces() -> anyhow::Result<()> {
+    let subscriber = get_subscriber("info")?;
+    init_subscriber(subscriber);
+    Ok(())
 }
--- src/yoda-taller/src/main.rs
+++ src/yoda-taller/src/main.rs
@@ -3,6 +3,11 @@ use yoda_taller::{server::startup::Application, settings::Settings};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    traces::init_prod_traces()?;
     let settings = Settings::read().context("cannot read settings")?;
-    Application::bind(settings)?.run().await
+    Application::bind(settings)?.run().await?;
+
+    opentelemetry::global::shutdown_tracer_provider();
+
+    Ok(())
 }
