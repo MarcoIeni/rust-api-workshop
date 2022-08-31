f0930d08a907df182eea5daf26cc47fb48565774
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2022-08-31 19:11:26 +0200

    rename var

--- src/yoda-taller/tests/api/helpers/test_traces.rs
+++ src/yoda-taller/tests/api/helpers/test_traces.rs
@@ -8,7 +8,7 @@ pub fn init_test_traces() {
 
 // Initialize the `tracing` stack once, using `once_cell`
 static TRACING: Lazy<()> = Lazy::new(|| {
-    if std::env::var("ENABLE_LOGS").is_ok() {
+    if std::env::var("TEST_LOG").is_ok() {
         let default_filter_level = "info";
         let subscriber = get_subscriber(default_filter_level).unwrap();
         init_subscriber(subscriber);
