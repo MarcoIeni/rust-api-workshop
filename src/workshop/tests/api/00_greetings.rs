b2191dc2850b1abcd76a277dfaed6a6f7cce19eb
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2022-09-03 14:09:17 +0200

    move test

---
+++ src/workshop/tests/api/00_greetings.rs
@@ -0,0 +1,18 @@
+//! Welcome to workshop!
+//! You will learn how to write Rust HTTP API.
+//!
+//! The material is structured as a series of exercises.
+
+/// This is your first exercise!
+///
+/// - Open your terminal
+/// - cd into the `src/workshop` directory
+/// - Solve the exercise
+/// - Run `cargo test` to check if tests pass or not.
+/// - Comment out the next module in the `src/workshop/src/lib.rs` files to progress.
+#[test]
+fn first_exercise() {
+    let i_am_ready_to_start = false;
+
+    assert!(i_am_ready_to_start);
+}
