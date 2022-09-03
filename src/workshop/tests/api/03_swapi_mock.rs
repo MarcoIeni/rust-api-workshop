33414a86ab8d5dd655275a1160c554b180d9a232
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2022-09-03 18:36:15 +0200

    wip

---
+++ src/workshop/tests/api/03_swapi_mock.rs
@@ -0,0 +1,4 @@
+//! You are able to retrieve data from Swapi, great!
+//! However a docker image of the service you are working with
+//! is not always available.
+//! Let's practice writing a mock!
