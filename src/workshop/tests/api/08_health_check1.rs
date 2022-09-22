f8b64474794a1ad610bcc236ec07719ccbbf74a7
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2022-09-22 12:45:07 +0200

    comment

--- src/workshop/tests/api/08_health_check1.rs
+++ src/workshop/tests/api/08_health_check1.rs
@@ -21,14 +21,17 @@ async fn health_check_works() {
         // With port 0, the operating system chooses a random port
         // among the free ones.
         application: ApplicationSettings { port: 0 },
-        // swapi settings are not important for this test
+        // Swapi settings are not important for this test.
         swapi: SwapiSettings {
             base_url: "http://127.0.0.1:9992".to_string(),
             timeout_milliseconds: 2000,
         },
     };
 
-    // the `Application::bind` method calls the `TcpListener::bind`
+    // The `Application::bind` method calls the
+    // [`TcpListener::bind`](https://doc.rust-lang.org/stable/std/net/struct.TcpListener.html#method.bind)
+    // to bind to the address `0.0.0.0:port`.
+    // The port needs to be read from the settings (in this case is 0).
     let application_bind = Application::bind(settings).unwrap();
     let port = application_bind.tcp_listener().local_addr().unwrap().port();
     let _app = tokio::spawn(application_bind.run());
