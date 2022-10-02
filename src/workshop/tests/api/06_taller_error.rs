639d6a6773007ce7075413c1b324b62d9a6274e2
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2022-10-02 10:34:24 +0200

    comment

--- src/workshop/tests/api/06_taller_error.rs
+++ src/workshop/tests/api/06_taller_error.rs
@@ -17,52 +17,13 @@ use {
     workshop::taller::YodaTallerError,
 };
 
-/// What happens when the user looks for a person that doesn't exist?
-/// Our server should return a 404 HTTP status code with an helpful message.
-/// Therefore the `is_taller_than` method should clearly distinguish this
-/// error case.
-/// We will call this type of error [`YodaTallerError::PersonNotFound`].
-#[tokio::test]
-async fn cannot_compare_yoda_and_non_existing_person() {
-    let app = TestApp::spawn().await;
-    let name = "Spock";
-
-    let body = swapi_mock::empty_query_result();
-    app.swapi_server.mock_people_query(name, body).await;
-    let is_taller_err = app.yoda_taller.is_taller_than(name).await.unwrap_err();
-    assert!(matches!(is_taller_err, YodaTallerError::PersonNotFound));
-}
-
-/// What happens when the user looks for a person with an unknown height?
-/// For example, try to use curl to retrieve the height of "Arvel Crynyd".
-///
-/// Again, the `is_taller_than` method should clearly distinguish this
-/// error case.
-/// We will use [`YodaTallerError::HeightNotFound`].
-#[tokio::test]
-async fn cannot_compare_yoda_and_person_with_invalid_height() {
-    let app = TestApp::spawn().await;
-    // Create the function `arvel`, which returns this `Person`:
-    // Person {
-    //     name: "Arvel Crynyd".to_string(),
-    //     height: "unknown".to_string(),
-    // }
-    // As you can see, Arvel has an unknown height.
-    // As Luke and Yoda, This person is present in Swapi.
-    let arvel = people::arvel();
-    let body = swapi_mock::person_query_result(&arvel);
-    app.swapi_server.mock_people_query(&arvel.name, body).await;
-    let is_taller_err = app
-        .yoda_taller
-        .is_taller_than(&arvel.name)
-        .await
-        .unwrap_err();
-    assert!(matches!(is_taller_err, YodaTallerError::HeightNotFound));
-}
-
 /// What happens if swapi behaves in weird ways, for example by
 /// returning invalid bytes in the response?
 /// Our library should return [`YodaTallerError::UnexpectedError`].
+///
+/// Add an `UnexpectedError` variant to the `YodaTallerError` enum.
+/// `UnexpectedError` should contain the original `reqwest::Error`
+/// of the `people_by_name` function.
 #[tokio::test]
 async fn return_decode_error_if_invalid_response() {
     let app = TestApp::spawn().await;
@@ -99,4 +60,47 @@ async fn return_timeout_error_if_timeout() {
         YodaTallerError::UnexpectedError(e) => assert!(e.is_timeout()),
         _ => panic!("unexpected error"),
     }
+}
+
+/// What happens when the user looks for a person that doesn't exist?
+/// Our server should return a 404 HTTP status code with an helpful message.
+/// Therefore the `is_taller_than` method should clearly distinguish this
+/// error case.
+/// We will call this type of error [`YodaTallerError::PersonNotFound`].
+#[tokio::test]
+async fn cannot_compare_yoda_and_non_existing_person() {
+    let app = TestApp::spawn().await;
+    let name = "Spock";
+
+    let body = swapi_mock::empty_query_result();
+    app.swapi_server.mock_people_query(name, body).await;
+    let is_taller_err = app.yoda_taller.is_taller_than(name).await.unwrap_err();
+    assert!(matches!(is_taller_err, YodaTallerError::PersonNotFound));
+}
+
+/// What happens when the user looks for a person with an unknown height?
+/// For example, try to use curl to retrieve the height of "Arvel Crynyd".
+///
+/// Again, the `is_taller_than` method should clearly distinguish this
+/// error case.
+/// We will use [`YodaTallerError::HeightNotFound`].
+#[tokio::test]
+async fn cannot_compare_yoda_and_person_with_invalid_height() {
+    let app = TestApp::spawn().await;
+    // Create the function `arvel`, which returns this `Person`:
+    // Person {
+    //     name: "Arvel Crynyd".to_string(),
+    //     height: "unknown".to_string(),
+    // }
+    // As you can see, Arvel has an unknown height.
+    // As Luke and Yoda, this person is present in Swapi.
+    let arvel = people::arvel();
+    let body = swapi_mock::person_query_result(&arvel);
+    app.swapi_server.mock_people_query(&arvel.name, body).await;
+    let is_taller_err = app
+        .yoda_taller
+        .is_taller_than(&arvel.name)
+        .await
+        .unwrap_err();
+    assert!(matches!(is_taller_err, YodaTallerError::HeightNotFound));
 }
