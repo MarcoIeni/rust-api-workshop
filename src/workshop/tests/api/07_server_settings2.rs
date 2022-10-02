978ea7dbfb091bdfbebe66c1a1bf477cc0027c8a
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2022-10-02 11:14:51 +0200

    comment

--- src/workshop/tests/api/07_server_settings2.rs
+++ src/workshop/tests/api/07_server_settings2.rs
@@ -20,8 +20,20 @@ swapi:
     // create a file that will be deleted after the test.
     let mut config_file = tempfile::NamedTempFile::new().unwrap();
     config_file.write_all(settings_yaml.as_bytes()).unwrap();
-    let config_file: &Path = config_file.path();
-    let actual_settings = Settings::read(config_file).unwrap();
+    let config_path: &Path = config_file.path();
+    // The `read` function reads the settings from a yaml file.
+    // This functions can fail in two ways:
+    // - The file doesn't exist
+    // - The file contains invalid yaml
+    //
+    // If we needed to distinguish the failure type, we could use the `thiserror` crate
+    // as we have done before.
+    // However, we don't care *why* the `read` function fails, because it's an
+    // unrecoverable error, that we just need to report to the developer that
+    // is trying to run the server.
+    // In these cases, you can use the
+    // [anyhow](https://docs.rs/anyhow/latest) crate.
+    let actual_settings: Settings = Settings::read(config_path).unwrap();
 
     let expected_settings = Settings {
         application: ApplicationSettings { port: 3000 },
