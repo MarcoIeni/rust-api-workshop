bfae551a4e42def21bf4c916f0460c13bfaa3022
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2023-03-13 23:30:11 +0000

    wip

--- src/workshop/tests/api/01_swapi_docker.rs
+++ src/workshop/tests/api/01_swapi_docker.rs
@@ -6,20 +6,29 @@
 /// To avoid calling directly https://swapi.dev/,
 /// run the swapi docker image locally.
 ///
-/// ```sh
-/// # Change directory to where you cloned https://github.com/MarcoIeni/swapi
-/// cd swapi
+/// - Option 1: Run the pre-built docker image (faster)
 ///
-/// # Build the docker image
-/// docker image build -t swapi .
+///   ```sh
+///   # The port 9992 must be free. You can use a different port if 9992 isn't free.
+///   docker run -p 9992:8000 -it ghcr.io/marcoieni/swapi
+///   ```
 ///
-/// # Run the docker image.
-/// # The port 9992 must be free. You can use a different port if 9992 isn't free.
-/// docker run -p 9992:8000 -it swapi
+/// - Option 2: Build the docker image yourself (slower)
 ///
-/// # You should see info about Luke Skywalker
+///   ```sh
+///   git clone https://github.com/MarcoIeni/swapi
+///   cd swapi
+///
+///   # Build the docker image
+///   docker image build -t swapi .
+///
+///   # Run the docker image.
+///   # The port 9992 must be free. You can use a different port if 9992 isn't free.
+///   docker run -p 9992:8000 -it swapi
+///   ```
+///
+/// You should see info about Luke Skywalker
 /// curl http://127.0.0.1:9992/api/people/1/
-/// ```
 ///
 /// Now you can also see Swapi docs by opening
 /// `http://127.0.0.1:9992/` in your browser.
