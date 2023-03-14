5d63e3ea3419e3de84c987a4ed386ec1bc115db0
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2023-03-14 14:21:21 +0000

    use query

--- src/yoda-taller/src/swapi.rs
+++ src/yoda-taller/src/swapi.rs
@@ -34,15 +34,19 @@ impl SwapiClient {
 
     #[instrument(skip(self))]
     pub async fn people_by_name(&self, name: &str) -> Result<Vec<Person>, reqwest::Error> {
-        let url = format!("{}/api/people/?search={name}", self.base_url);
         let query: Query = self
             .http_client
-            .get(&url)
+            .get(&self.people_url())
+            .query(&[("search", name)])
             .send()
             .await?
             .error_for_status()?
             .json()
             .await?;
         Ok(query.results)
+    }
+
+    fn people_url(&self) -> String {
+        format!("{}/api/people/", self.base_url)
     }
 }
