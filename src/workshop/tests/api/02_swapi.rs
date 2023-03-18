41f9cb7b33a2be3e927821ad7597bd644468e1a8
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2023-03-18 14:46:34 +0100

    add an additional test to do network request

--- src/workshop/tests/api/02_swapi.rs
+++ src/workshop/tests/api/02_swapi.rs
@@ -7,22 +7,11 @@ use std::time::Duration;
 //   in `src/workshop/src/swapi.rs` to re-use them in the next exercises.
 use workshop::swapi::{Person, SwapiClient};
 
-/// Query the swapi instance running in docker to retrieve
-/// Luke's height.
+/// As a general rule, you should focus on making the test compile and pass first by mocking your functions.
+/// For example, for this exercise implement the `people_by_name` function
+/// so that it returns Luke's `Person` struct, without doing any network call.
 ///
-/// ## Useful resources 📚
-/// - [Search query - Swapi docs](https://swapi.dev/documentation#search):
-///   documentation of the API call you need to make.
-/// - [reqwest](https://docs.rs/reqwest/): the Rust HTTP client library.
-/// - [serde](https://serde.rs/): the library to deserialize JSON responses from swapi.
-///
-/// ## Hint 1 💡
-/// Always focus on making the test compile and pass first by mocking your functions.
-/// For example, `people_by_name` could just return the expected `Person` struct, without doing any network call at first.
-/// Please, make sure to use `reqwest` to create the HTTP client and retrieve
-/// Luke's height before jumping to the next test.
-///
-/// ## Hint 2 💡
+/// ## Hint 💡
 /// This test doesn't have `todo!()`, so you don't have to edit it to make it pass.
 #[tokio::test]
 async fn retrieve_luke_height_from_swapi() {
@@ -35,6 +24,28 @@ async fn retrieve_luke_height_from_swapi() {
     let luke = Person {
         name: "Luke Skywalker".to_string(),
         height: "172".to_string(),
+    };
+    let people: Vec<Person> = swapi_client.people_by_name(&luke.name).await.unwrap();
+    assert_eq!(people, vec![luke]);
+}
+
+/// We have the basic structure of the `SwapiClient` struct.
+/// Now edit the `people_by_name` function: use `reqwest` to create the HTTP client
+/// and retrieve R2-D2's height from the swapi API.
+///
+/// ## Useful resources 📚
+/// - [Search query - Swapi docs](https://swapi.dev/documentation#search):
+///   documentation of the API call you need to make.
+/// - [reqwest](https://docs.rs/reqwest/): the Rust HTTP client library.
+/// - [serde](https://serde.rs/): the library to deserialize JSON responses from swapi.
+async fn retrieve_rd_d2_height_from_swapi() {
+    let base_url = "http://127.0.0.1:9992";
+    // You can ignore the timeout for this exercise.
+    let timeout = Duration::from_secs(2);
+    let swapi_client = SwapiClient::new(base_url.to_string(), timeout).unwrap();
+    let luke = Person {
+        name: "r2-d2".to_string(),
+        height: "96".to_string(),
     };
     let people: Vec<Person> = swapi_client.people_by_name(&luke.name).await.unwrap();
     assert_eq!(people, vec![luke]);
